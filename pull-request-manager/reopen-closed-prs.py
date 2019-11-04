cf7b48812c8cc4c10a4179b50e40a156a9a3f1f0
George Webb <george.webb@oxbotica.com>
2019-11-04 14:08:20 +0000

    add remove all flags to open or closed PRs

--- pull-request-manager/reopen-closed-prs.py
+++ pull-request-manager/reopen-closed-prs.py
@@ -2,29 +2,42 @@ from github import Github
 
 REPO_NAME = "george-webb-oxb/test-repo"
 
+
 def reopen_the_pr(pr, CLOSED_LABEL):
     print('reopening the pr')
     comment_message = 'This PR is being reopened'
     pr.create_issue_comment(comment_message)
 
-    # remove closed label
-    pr.remove_from_labels(CLOSED_LABEL)
-            
+    # remove all
+    pr.delete_labels()
+
     print('pr reopeneed')
     pr.edit(state='open')
     return
+
 
 def reopen_all_prs(repo, CLOSED_LABEL):
     '''reopens all prs in this repo'''
 
     # get an object with all pull requests
     pulls = repo.get_pulls(state='closed', sort='created', base='master')
-
     print('This repo has {} closed pull requests'.format(pulls.totalCount))
     # loop through the pull requests
     for pr in pulls:
         reopen_the_pr(pr, CLOSED_LABEL)
     return
+
+
+def remove_all_labels(repo):
+    '''removes all labels in open prs in this repo'''
+
+    # get an object with all pull requests
+    pulls = repo.get_pulls(state='open', sort='created', base='master')
+    print('This repo has {} open pull requests'.format(pulls.totalCount))
+    # loop through the pull requests
+    for pr in pulls:
+        pr.delete_labels()
+
 
 def main():
     # get an access token from file
@@ -41,6 +54,8 @@ def main():
     repo = gh.get_repo(REPO_NAME)
 
     reopen_all_prs(repo, CLOSED_LABEL)
+    remove_all_labels(repo)
+
 
 if __name__ == "__main__":
     main()
